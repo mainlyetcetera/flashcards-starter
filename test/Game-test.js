801d9fc1fa9361c4387720ca5a03d6dbb7fc3678
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-03 19:36:56 -0700

    Add requirements to game test file

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,2 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const Round = require('../src/Round');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
