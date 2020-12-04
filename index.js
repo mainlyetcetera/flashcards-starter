62864808943236870c3e1fb649b7090e0b937fa4
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-03 20:54:27 -0700

    Add beginning of attempt to run game

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const http = require('http');
 let app = http.createServer();
+// const Game = require('./src/Game');
+// const Deck = require('./src/Deck');
+// const Round = require('./src/Round');
+// const Card = require('./src/Card');
+// const Turn = require('./src/Turn');
+
+// const deck = 
+// const game = new Game
 
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');  
