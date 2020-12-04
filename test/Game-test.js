d9c5f9cd40565bdaee60350fae35f2f81e3ac371
mainlyetcetera <ericcampbell294@gmail.com>
2020-12-03 19:42:00 -0700

    Expect currentRound to be an instance of Round

--- test/Game-test.js
+++ test/Game-test.js
@@ -5,3 +5,26 @@ const Round = require('../src/Round');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
+
+describe('Game', () => {
+  let game;
+
+  beforeEach(() => {
+    card1 = new Card(1, '4 + 5', ['6', '7', '9'], '9');
+    card2 = new Card(2, '1 + 2', ['3', '4', '5'], '3');
+    card3 = new Card(3, 'stuff or things', ['stuff', 'things', 'et cetera'], 'stuff');
+    cards = [card1, card2, card3];
+    
+    deck = new Deck(cards);
+    
+    turn = new Turn(card1, '9');
+    guess = turn.answer;
+
+    round = new Round(deck);
+    game = new Game(deck, round);
+  });
+
+  it.skip('should have currentRound property', () => {
+    expect(game.round).to.be.an.instanceof(Round);
+  });
+});
